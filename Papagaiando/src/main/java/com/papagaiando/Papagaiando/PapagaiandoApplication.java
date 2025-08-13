35dfc6436cb71fde2feb517ee35405482b93e002
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-08-13 08:47:32 -0300

    Inicializando projeto backend

---
+++ Papagaiando/src/main/java/com/papagaiando/Papagaiando/PapagaiandoApplication.java
@@ -0,0 +1,13 @@
+package com.papagaiando.Papagaiando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapagaiandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapagaiandoApplication.class, args);
+	}
+
+}
---
+++ Papagaiando/src/test/java/com/papagaiando/Papagaiando/PapagaiandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papagaiando.Papagaiando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapagaiandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
