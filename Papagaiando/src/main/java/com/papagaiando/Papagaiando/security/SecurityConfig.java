5385cfc20be1f9c5c68f13b60b6124a50407b513
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-09-03 11:18:09 -0300

    adicionando cors

--- Papagaiando/src/main/java/com/papagaiando/Papagaiando/security/SecurityConfig.java
+++ Papagaiando/src/main/java/com/papagaiando/Papagaiando/security/SecurityConfig.java
@@ -27,6 +27,17 @@ public class SecurityConfig {
 @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+
+    .cors(cors -> cors
+                        .configurationSource(request -> {
+                            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                            corsConfig.setAllowedOrigins(java.util.List.of("http://localhost:5173/"));
+                            corsConfig.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                            corsConfig.setAllowedHeaders(java.util.List.of("*"));
+                            corsConfig.setAllowCredentials(Boolean.TRUE);
+                            return corsConfig;
+                        })
+                )
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/auth/login", "/usuarios").permitAll()
